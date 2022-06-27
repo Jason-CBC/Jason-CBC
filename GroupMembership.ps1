fd0aea5785f9e1b167f40e273e8e5c21aef0d03f
Jason-CBC <94542234+Jason-CBC@users.noreply.github.com>
2022-06-27 12:03:38 -0400

    Delete GroupMembership.ps1

--- GroupMembership.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Name:  Group Membership
-# Created By:  Jason Sowers
-# Date:  June 23, 2022
-#
-$GroupName = "App.Corp.FEPDirect"
-Get-ADGroupMember -Identity $GroupName -Recursive | select SAMAccountName, name | Export-Csv GroupUserList.csv
